cb2c59eaa3f17bc68ececa9158f9c70ddfd0c5b6
firdavs <firdavsolimjonov25@gmail.com>
2022-12-14 16:47:56 +0500

    added

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -1,13 +1,25 @@
 package com.epam.rd.autotasks.intersection;
 
 public class Line {
+    int k;
+    int b;
 
     public Line(int k, int b) {
-
+        this.k = k;
+        this.b = b;
     }
 
     public Point intersection(Line other) {
-        throw new UnsupportedOperationException();
+        int x , y;
+
+        try {
+            x = (other.b - this.b)/(this.k - other.k);
+            y = this.k * x + this.b;
+        }catch (ArithmeticException e){
+            return null;
+        }
+
+        return new Point(x,y);
     }
 
 }
